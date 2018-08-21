eeda3410b746372598ab7b31a3637d2f3b58344d
gblatstein@gmail.com <gblatstein@gmail.com>
2018-08-21 17:54:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-function takeANumber(number,name){
-  if (number===0)
-    return "Welcome," + `${name}`+". You are number" + `${number}`+ "in line"
-}
+function currentLine(arr) {
+  var empty = [];
+  if (arr.length < 1) {
+    return 'There is nobody in line.';
+  } else {
+    for (var i = 0; i < arr.length; i++) {
+      empty.push(arr.indexOf(arr[i]) + 1, '.', arr[i], ', ');
+    }
+    return 'The line is currently: ' + empty.join(' ');
+  }
